28c57ccd2f141372417dd9cfeffe47766c096dae
seikensk <seiken.sk@gmail.com>
2020-06-06 14:49:45 +0800

    2

--- lesson2/substrate-node-template/pallets/poe/src/lib.rs
+++ lesson2/substrate-node-template/pallets/poe/src/lib.rs
@@ -4,8 +4,9 @@
 
 
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch,ensure};
 use frame_system::{self as system, ensure_signed};
+use sp_std::prelude::*;
 
 #[cfg(test)]
 mod mock;
@@ -27,27 +28,24 @@ decl_storage! {
 	// storage items are isolated from other pallets.
 	// ---------------------------------vvvvvvvvvvvvvv
 	trait Store for Module<T: Trait> as TemplateModule {
-		Proofs get(fn Proofs ) map haser(black2_128_concat)Vec<u8> => (T::AccountId, T::BlockNumber);
+		Proofs get(fn proofs ): map hasher(black2_128_concat)Vec<u8> => (T::AccountId, T::BlockNumber);
 	}
 }
 
 // The pallet's events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		/// Just a dummy event.
-		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
+		Claimcreated(AccountId,Vec<u8>),
+		ClaimRevoked(AccountId,Vec<u8>),
 	}
 );
 
 // The pallet's errors
 decl_error! {
 	pub enum Error for Module<T: Trait> {
-		/// Value was None
-		NoneValue,
-		/// Value reached maximum and cannot be incremented further
-		StorageOverflow,
+		ProofAlreadyExist,
+		ClaimNotExist,
+		NotclaimOwner
 	}
 }
 
@@ -64,38 +62,35 @@ decl_module! {
 		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
 
-		/// Just a dummy entry point.
-		/// function that can be called by the external world as an extrinsics call
-		/// takes a parameter of the type `AccountId`, stores it, and emits an event
-		#[weight = 10_000]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let who = ensure_signed(origin)?;
+		#[weight = 0]
+		pub fn create_claim(origin,claim:Vec<u8>) ->dispatch::DispatchResult {
+			let sender =ensure_signed(origin)?;
 
-			// Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+			ensure!(Proofs::<T>::contains_key(&claim),Error::<T>::ProofAlreadyExist);
 
-			// Here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			Proofs::<T>::insert(&claim,(sender.clone(),system::Module::<T>::block_number()));
+
+			self::deposit_event(RawEvent::Claimcreated(sender,claim));
+			
 			Ok(())
 		}
 
-		/// Another dummy entry point.
-		/// takes no parameters, attempts to increment storage value, and possibly throws an error
-		#[weight = 10_000]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let _who = ensure_signed(origin)?;
+		#[weight = 0]
+		pub fn revoke_claim(orgin,claim:Vec<u8>) ->dispatch::DispatchResult{
+			let sender = ensure_signed(origin)?;
 
-			match Something::get() {
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					Something::put(new);
-					Ok(())
-				},
-			}
+			ensure!(Proofs::<T>::contains_key(claim),Error::<T>::ClaimNotExist);
+
+			let (owner,block_number) = Proofs::<T>::get(&claim);
+
+			ensure!(owner == sender,Error::<T>::NotclaimOwner);
+
+			Proofs::<T>::remove(&claim);
+
+			Self::deposit_event(RawEvent::ClaimRevoked(sender,claim));
+
+			Ok(())
 		}
+		
 	}
 }
